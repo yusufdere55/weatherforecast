9fa08ba6f454e1dde03ea0d39783c7ac95de9a71
Yusuf Dere <151437333+yusufdere55@users.noreply.github.com>
2025-03-08 22:38:12 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,14 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
-  base: '/weatherforecast/',
+  resolve: {
+    alias: {
+      '@': '/src'
+    }
+  },
+  build: {
+    outDir: 'dist'
+  },
   server: {
     proxy: {
       '/api': {
